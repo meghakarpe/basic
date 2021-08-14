028b07f3bb51d3dd47d41a9c48ac5305a64685f6
meghakarpe <88928087+meghakarpe@users.noreply.github.com>
2021-08-14 15:20:31 +0530

    Add files via upload

---
+++ q.py
@@ -0,0 +1,8 @@
+i=1
+sum=0
+while i<=10:
+    if i%2!=0:
+        print(i)
+        sum=sum+i
+        i=i+1
+print("sum of 1 to 10",sum)
---
+++ question operator.py
@@ -0,0 +1,195 @@
+# a=24
+# b=36
+# print(a//b)
+
+# a=6**2+4%3+2//3+3+8
+# print(a)
+
+# a=9+8+9**2+9//3+3*2
+# print(a)
+
+# a=6//3+4**3+8%2+3-4
+# print(a)
+
+# 123abc="19.1"
+# print(int(123abc))
+# 
+# @varsha=456
+# print(float(@varsha))
+
+# Raj="Dev"
+# abc=int(Raj)
+# print(type(abc))
+# print(abc)
+
+# a=24
+# b=36
+# print(a*b)
+# print(a%b)
+# print(a>36)
+# print(a!=b)
+
+# a=10
+# b=15
+# c=2
+# d=a**c//10+a
+# print(d)
+# e=b%c+12-15/2
+# print(e)
+# f=1**5+a%c-20//5
+# print(f)
+# g=a+b/c
+# print(g)
+
+# a=20
+# b=20
+# print(a<b and b>a)
+
+# a=20
+# b=10
+# print(a>b and b>a)
+# print(b>=5 and a<=10)
+# print(10==b and 5<=b)
+# print(b==a or a!=20)
+
+# a=10122020
+# print(a)
+
+# A=True and False or True and True and False and False or True or False
+# print(A)
+
+# A=True and False or False and not True
+# print(A)
+
+# A=True or False and True and False
+# print(A)
+
+# M=True or False and True or True
+# print(M)
+
+# a=10
+# b=20
+# c=30
+# d=40
+# e=25
+# print(a//b**c+b-d*e%a)
+
+# a=26//7**2%6+4-6/8*8
+# print(a)
+
+# a=26
+# b=1
+# print(a//b)
+
+# a=6
+# b=64
+# print(6/64)
+
+# a=4**6%7//6/4*3-4+8
+# print(a)
+# 
+# a=84+6**3%4
+# print(a)
+
+# a="10"
+# b="6"
+# print(a*40)
+# print(a//b*3)
+# print(a-b)
+
+# a=5
+# a=a*"5"
+# print(a)
+
+# a=5
+# a=a*"5"
+# print(type(a))
+
+# num1="New"
+# num2="number"
+# print(type(num1+num2))
+
+# x=20
+# y=5
+# result=(x+True+y-False)
+# print(result)
+
+# str3='''Hello'''
+# print(str3)
+
+# a=2-(-1+7*3)/(6-7)*(-1)
+# print(a)
+
+# a=9*7%5*0+9//5+2
+# print(a)
+
+# print(10%10//2/10)
+
+# print(-16/4-7.0)
+
+# print(-16/4)
+
+# print(-21/3)
+
+# print(-18/1176800)
+
+# print(-18/1)(25*2%2**3%1768*100)
+
+# a=True
+# b=12
+# print(a+b)
+
+# a="True"
+# b=12
+# print(a+b)
+
+# print(4**6%7//6/4*3-4+8)
+
+# print(10//20**30+20-40*25%10)
+
+# print(26//7**2%6+4-6/64)
+
+# print(26//49%6+4-0.75*8)
+
+# print(26//49%6+4-0.75*8)
+
+# print(5%8//9-5+10*15+10)
+
+# a="10/"
+# b="12/"
+# c="2020"
+# print(a+b+c)
+
+# a=10<12<2020
+# print(a)
+
+# a=10
+# a+=12
+# print(a)
+
+# a="10/"
+# a+="12/" 
+# a+="2020"
+# print(a)
+
+# a="10/"
+# b="12/"
+# c="2020"
+# print(a,b,c)
+
+# a="10/"
+# b="12/"
+# c="2020"
+# print(a+b+c)
+
+# a=10
+# b=5
+# a,b=b,a
+# print(a,b)
+
+# input("Enter the number")
+
+
+
+
+
