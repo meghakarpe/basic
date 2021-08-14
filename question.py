2747b6b762fc2bb4d0f955e53dab5376239a254a
meghakarpe <88928087+meghakarpe@users.noreply.github.com>
2021-08-14 15:18:59 +0530

    Add files via upload

---
+++ question.py
@@ -0,0 +1,113 @@
+
+# printa=megha
+# print
+
+# a=20
+# b="megha"
+# print(a*3,b)
+
+# a="megha"
+# print(a*3)
+
+# Name="prity"
+# Nam=5
+# print(str(Nam)+Name)
+
+# new_name_5="Megha"
+# place="pune"
+# print(new_name_5/place)
+
+# var_1=2+0j
+# print(var_1)
+
+a=15
+b="5"
+# print(a+"b")
+# print(b+str(a))
+# print("a"+"b")
+# print(a+"b")
+# print("a"+b)
+# print(a+str(a))
+# print(b+"int(a)")
+# print(a+"int(b)")
+
+# a="Neha"
+# a="Randhavan"
+# print(a)
+
+# x=10
+# y=complex(x)
+# print(y)
+
+# x=10
+# print(complex(x))
+
+# a=100.5
+# print(int(100.5))
+
+# a=8+7j
+# print(int(a))
+
+# a=8+7j
+# print(str(a))
+
+# a=7
+# b="2"
+# c=8.1
+# print(str(a)+int(b)+c)
+
+# a=7
+# b="2"
+# c=8.1
+# print(str(a)+b+str(c))
+
+# a=7.5
+# b=6.5
+# print(a+b)
+
+# a="7.5"
+# b="6.5"
+# print(a+b)
+
+# a="7.5"
+# b="6.5"
+# print(a*b)
+
+# a20=
+b="15"
+# print(a+b)
+
+# a="pooja"
+# b="Neha"
+# c="chaya"
+# b="priya"
+# print(a*b*c*b)
+
+x=5
+y="Johan"
+# print(type(x))
+# print(y)
+# print(type
+# num=0
+# num1=num+10
+# num2=num1+num
+# num3=num2+1
+# print(num1,num2,num3)
+
+# var12='20'
+# var13=float(var12)
+# print(type(var12))
+
+a="1"
+b=3
+# print(a+b)
+# print(str(b)+a)b
+# print(a+"int(b)")
+# print(b+int(b))
+# print(b+int(a))
+
+# a="Nikita"
+# b="arti"
+# print(a+b)
+
+
